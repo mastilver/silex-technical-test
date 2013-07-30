a33f5fcec3e4bc50158c33d7fe9402a3fb54e3b0
Sileghem Thomas <th.sileghem@gmail.com>
2013-07-30 14:25:04 +0200

    setup finished

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,9 +6,28 @@ $app = new Silex\Application();
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-        'driver'   => 'pdo_sqlite',
-        'path'     => __DIR__.'/app.db',
+        'driver'   => 'pdo_pgsql',
+        'dbname' => 'tech_test',
+        'user' => 'silex',
+        'password' => 'silex',
     ),
+));
+
+$app->register(new Nutwerk\Provider\DoctrineORMServiceProvider(), array(
+    'db.orm.proxies_dir'           => __DIR__ . '/cache/doctrine/proxy',
+    'db.orm.proxies_namespace'     => 'DoctrineProxy',
+    'db.orm.cache'                 => 
+        !$app['debug'] && extension_loaded('apc') ? new ApcCache() : new ArrayCache(),
+    'db.orm.auto_generate_proxies' => true,
+    'db.orm.entities'              => array(array(
+        'type'      => 'annotation',       // entity definition 
+        'path'      => __DIR__ . '/src',   // path to your entity classes
+        'namespace' => 'MyWebsite\Entity', // your classes namespace
+    )),
+));
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/views',
 ));
 
 $app['debug'] = true;
